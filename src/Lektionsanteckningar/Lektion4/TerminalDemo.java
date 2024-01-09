8211ce9adf9b74c138db0bfdbe5f1322b9ba8dae
André Aldosson <andre.aldosson@hotmail.se>
2024-01-09 11:25:37 +0100

    övningar yao

--- src/Lektionsanteckningar/Lektion4/TerminalDemo.java
+++ src/Lektionsanteckningar/Lektion4/TerminalDemo.java
@@ -1,13 +1,30 @@
 package Lektionsanteckningar.Lektion4;
 
-import java.io.*;
+// STRÖMMAR.
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 class TerminalDemo {
-    public static void main(String[] arg) throws IOException {
-        BufferedReader input = new BufferedReader(
-                new InputStreamReader(System.in));
-        System.out.print("Vad heter du? ");
-        System.out.flush();
-        String txt = input.readLine();
-        System.out.println("Hej " + txt);
+    public static void main(String[] args) throws IOException {
+        System.out.println("Mata in ett heltal och tryck på Enter/Retur ");
+        System.out.println("Avsluta med CTRL+Z eller CTRL+D (Win) CMD+D (Mac) ");
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0; // lagrar summan av alla inlästa tal
+        int highest = Integer.MIN_VALUE;
+        while (true){
+            String txt = input.readLine();
+            if (txt == null || txt.isEmpty())
+                break;
+            int num = Integer.parseInt(txt);
+            sum += num;
+            if (num > highest) {
+                highest = num;
+            }
+        }
+        System.out.println("Högsta inmatade värdet är: " + highest);
+
     }
 }
